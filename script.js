09c2fd9cb9372d5468647ecf8f2da5bdb84bc21a
Kuldeep kumar <117909055+kd-234@users.noreply.github.com>
2024-04-18 12:28:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //your JS code here. If required.
-var currentURL = window.location.href;
-var n = currentURL.length;
 
-alert("The length of the URL is" + n);
+		// Get the current URL
+		let currentUrl = window.location.href;
+
+		// Get the length of the URL
+		let urlLength = currentUrl.length;
+
+		// Log the length of the URL to the console
+		alert("The length of the URL is: " + urlLength);
+
+	
